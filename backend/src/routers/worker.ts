ba9c1c2e917fe038f5811bc99d16e21e59e55eeb
b-u-g-g <tsakshi1708@gmail.com>
2025-04-07 01:02:46 +0530

    worker file

--- backend/src/routers/worker.ts
+++ backend/src/routers/worker.ts
@@ -1,7 +1,20 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 
 const router = Router();
+const TOTAL_SUBMISSIONS = 100;
+const prismaClient = new PrismaClient();
+
+prismaClient.$transaction(
+    async (prisma) => {
+      // Code running in a transaction...
+    },
+    {
+      maxWait: 5000, // default: 2000
+      timeout: 10000, // default: 5000
+    }
+)
 
 router.post("/signin", async(req, res) => {
 
